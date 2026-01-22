094c9e4804d959ccface6ee1f9a822821322ffc8
Kolyn090 <kolynlin@protonmail.com>
2026-01-22 13:28:08 -0500

    Upgrade weapon extraction complete

--- src/Run.java
+++ src/Run.java
@@ -18,7 +18,7 @@ public class Run {
          */
 
         try {
-            String folder = "lib/hunting-horn";
+            String folder = "lib/bow";
             File file = new File(folder + "/map.txt");
             Scanner scanner = new Scanner(file);
             GraphicGraph graph = new GraphicGraph();
